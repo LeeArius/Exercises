8316de9960c3734ee75cb749df34f968d1ed79ac
Finn_ <leearius.erana@cvsu.edu.ph>
2022-07-14 14:06:41 +0800

    additions

--- storefront/storefront/playground/views.py
+++ storefront/storefront/playground/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# request -> response
+# request handler
+
+def calculate():
+    x = 1
+    y = 2
+    return x
+def say_hello(request):
+    x = calculate()
+    return render(request, 'hello.html', {'name': 'Mosh'})
