4441cff7922881f4ce93e0e4b352c28cdcb85945
Finn_ <leearius.erana@cvsu.edu.ph>
2022-07-20 15:56:06 +0800

    CRUD app

--- exercises/employee_project/employee_project/settings.py
+++ exercises/employee_project/employee_project/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'employee_register'
+    'employee_register',
+    
 ]
 
 MIDDLEWARE = [
@@ -76,10 +77,8 @@ WSGI_APPLICATION = 'employee_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'EmployeeDB',
-        'USER': 'postgres',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        "NAME": 'db.sqlite3'
     }
 }
 
--- exercises/employee_project/employee_project/urls.py
+++ exercises/employee_project/employee_project/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from employee_register import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    re_path(r'^create$', views.create, name='create'),
+    re_path(r'^edit/(?P<id>\d+)$', views.edit, name='edit'),
+    re_path(r'^edit/update/(?P<id>\d+)$', views.update, name='update'),
+    re_path(r'^delete/(?P<id>\d+)$', views.delete, name='delete'),
 ]
